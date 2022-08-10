11177fc5c8746335a9e023de15ee1074924259c9
Le Dinh Tho <ThoLD@vietis.vn>
2022-08-10 18:10:53 +0700

    muoi thang tam

---
+++ app.js
@@ -0,0 +1,70 @@
+// ----------- Slider banner ---------------
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSliderBanner(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("banner-content__item");
+  let dots = document.getElementsByClassName(
+    "banner-bottom__item--line__process"
+  );
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    console.log("dots[i].className", dots[i].className);
+    dots[i].className = dots[i].className.replace(" banner-slider__active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " banner-slider__active";
+}
+
+// Collapse
+
+$(".collapse-toggle").click(function (e) {
+  e.preventDefault();
+  $("a:first-of-type").removeClass("chev");
+
+  var $this = $(this);
+  if ($this.next().hasClass("show")) {
+    $this.next().removeClass("show");
+    $this.next().slideUp(350);
+  } else {
+    $this.parent().parent().find(".collapse-content").removeClass("show");
+    $this.parent().parent().find(".collapse-content").slideUp(350);
+    $this.toggleClass("chev");
+    $this.next().toggleClass("show");
+    $this.next().slideToggle(350);
+  }
+});
+
+// Slider Service
+
+$(document).ready(function () {
+  $(".services-slider").slick({
+    slidesToShow: 1,
+    dots: false,
+    arrows: true,
+    infinite: true,
+  });
+});
+
+$(document).ready(function () {
+  $(".teams-slider").slick({
+    slidesToShow: 1,
+    dots: false,
+    arrows: false,
+    infinite: true,
+  });
+});
